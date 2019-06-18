289ffd4d1495d40049bd38e0e0e6d5683148fde4
rougsig <rougsig@gmail.com>
2019-06-19 01:14:47 +0200

    Add Deprecation to out and sc

--- src/main/kotlin/com/github/rougsig/core/IOEnvironment.kt
+++ src/main/kotlin/com/github/rougsig/core/IOEnvironment.kt
@@ -7,9 +7,12 @@ class IOEnvironment(
   private val input: InputStream,
   private val output: OutputStream
 ) {
+  @Deprecated("use sb with printResult. sb faster")
   val out by lazy(LazyThreadSafetyMode.NONE) { PrintStream(output) }
-  val rd by lazy(LazyThreadSafetyMode.NONE) { BufferedReader(InputStreamReader(input)) }
+  @Deprecated("use rd with StringTokenizer. rd faster")
   val sc by lazy(LazyThreadSafetyMode.NONE) { Scanner(input) }
+
+  val rd by lazy(LazyThreadSafetyMode.NONE) { BufferedReader(InputStreamReader(input)) }
   val sb by lazy(LazyThreadSafetyMode.NONE) { LinkedList<String>() }
 
   fun printResult() = output.write(sb.joinToString("\n").toByteArray())
