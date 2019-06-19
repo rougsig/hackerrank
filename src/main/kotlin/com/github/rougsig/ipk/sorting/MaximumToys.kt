60ae4ae1a1f7a6ddc5113e3a085e879e9464bd89
rougsig <rougsig@gmail.com>
2019-06-19 23:01:48 +0200

    Add solution for `Mark and Toys`

--- src/main/kotlin/com/github/rougsig/ipk/sorting/MaximumToys.kt
+++ src/main/kotlin/com/github/rougsig/ipk/sorting/MaximumToys.kt
@@ -1,7 +1,29 @@
 package com.github.rougsig.ipk.sorting
 
 import com.github.rougsig.core.IOEnvironment
+import java.util.*
 
 fun IOEnvironment.maximumToys() {
+  var tokens = StringTokenizer(rd.readLine())
+
+  val n = tokens.nextToken().toInt()
+  var amount = tokens.nextToken().toInt()
+
+  tokens = StringTokenizer(rd.readLine())
+
+  val arr = IntArray(n) { tokens.nextToken().toInt() }
+  arr.sort()
+
+  var counter = 0
+  for (i in 0..arr.lastIndex) {
+    if (amount >= arr[i]) {
+      amount -= arr[i]
+      counter += 1
+    } else {
+      break
+    }
+  }
+
+  sb.add("$counter")
   printResult()
 }
