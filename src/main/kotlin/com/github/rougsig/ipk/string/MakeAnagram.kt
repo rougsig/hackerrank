7216e83bdb17c6148a96a9cdc3aa7e6403558dd5
rougsig <rougsig@gmail.com>
2019-07-01 00:12:40 +0200

    Add solution for `Strings: Making Anagrams`

--- src/main/kotlin/com/github/rougsig/ipk/string/MakeAnagram.kt
+++ src/main/kotlin/com/github/rougsig/ipk/string/MakeAnagram.kt
@@ -2,6 +2,17 @@ package com.github.rougsig.ipk.string
 
 
 import com.github.rougsig.core.IOEnvironment
+import kotlin.math.abs
 
 fun IOEnvironment.makeAnagram() {
+  val s1 = nextToken()
+  val s2 = nextToken()
+
+  val arr = IntArray(26)
+
+  s1.forEach { arr[it - 'a'] += 1 }
+  s2.forEach { arr[it - 'a'] -= 1 }
+
+  addToResult(arr.sumBy { abs(it) }.toString())
+  printResult()
 }
