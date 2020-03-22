2722744ee5b5e9e08ee8dad51295839906786019
rougsig <rougsig@gmail.com>
2020-03-22 22:19:10 +0200

    Add luckBalance solution

--- src/main/kotlin/com/github/rougsig/ipk/greedy/LuckBalance.kt
+++ src/main/kotlin/com/github/rougsig/ipk/greedy/LuckBalance.kt
@@ -5,4 +5,14 @@ import com.github.rougsig.core.IOEnvironment
 fun IOEnvironment.luckBalance() {
   val n = nextToken().toInt()
   val k = nextToken().toInt()
+  val contests = (0 until n)
+    .map { nextToken().toInt() to (nextToken().toInt() == 1) }
+  val minLosses = contests
+    .filter { (_, it) -> it }
+    .sortedByDescending { (it, _) -> it }
+    .drop(k)
+    .sumBy { (it, _) -> it }
+  val totalValue = contests.sumBy { (it, _) -> it }
+  addToResult((totalValue - minLosses * 2).toString())
+  printResult()
 }
